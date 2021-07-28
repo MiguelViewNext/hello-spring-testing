046ee4bd8f3df32f350c74114ae4012e32b30911
Miguel Herrero <alguiencualquiera@gmail.com>
2021-07-28 11:37:42 +0200

    Modificado archivo HelloSpringApplication

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Build stage
 FROM openjdk:11 AS base
-WORKDIR /opt/hello-gradle
+WORKDIR /opt/hello-spring
 COPY ./ ./
 RUN ./gradlew assemble \
   && rm -rf /root/.gradle
@@ -10,6 +10,6 @@ RUN ./gradlew assemble \
 # FROM adoptopenjdk:11-jre-hotspot
 #FROM openjdk:11.0.12-jre-slim-buster
 FROM openjdk:11.0-jre-slim
-WORKDIR /opt/hello-gradle
-COPY --from=base /opt/hello-gradle/build/libs/demo-0.0.1-SNAPSHOT.jar ./
-CMD java -jar demo-0.0.1-SNAPSHOT.jar
+WORKDIR /opt/hello-spring
+COPY --from=base /opt/hello-spring/build/libs/hello-spring-0.0.1-SNAPSHOT.jar ./
+CMD java -jar hello-spring-0.0.1-SNAPSHOT.jar
--- src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
+++ src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
@@ -1,4 +1,4 @@
-package com.example.hellospring
+/*package com.example.hellospring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -10,4 +10,26 @@ class HelloSpringApplication {
         SpringApplication.run(HelloSpringApplication, args)
     }
 
+}*/
+
+package com.example.hellospring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args)
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name)
+    }
 }
